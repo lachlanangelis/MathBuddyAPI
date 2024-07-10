c9219b7f752d607c88473a49602a9f636a831049
Zuhayer Aousaf <52472584+xuhayer@users.noreply.github.com>
2024-07-10 13:50:27 +1000

    Update app.py
    added routes for database tables

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS
+import MySQLdb.cursors
 import ollama
 
 app = Flask(__name__)
@@ -86,6 +87,82 @@ def login():
         return jsonify({"message": "Login successful"}), 200
     else:
         return jsonify({"message": "Invalid credentials"}), 401
+
+#Database Query functions and routes
+def fetch_data_from_table(table_name):
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute(f"SELECT * FROM {table_name}")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+@app.route('/tables', methods=['GET'])
+def get_tables():
+    cur = mysql.connection.cursor()
+    cur.execute("SHOW TABLES")
+    tables = cur.fetchall()
+    table_list = [table[0] for table in tables]
+    cur.close()
+    return jsonify(table_list)
+
+@app.route('/tables/<table_name>', methods=['GET'])
+def get_table_data(table_name):
+    try:
+        data = fetch_data_from_table(table_name)
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+#To access the data use /tables/<tablename>
+@app.route('/classes', methods=['GET'])
+def get_classes():
+    data = fetch_data_from_table('classes')
+    return jsonify(data)
+
+@app.route('/feedback', methods=['GET'])
+def get_feedback():
+    data = fetch_data_from_table('feedback')
+    return jsonify(data)
+
+@app.route('/lessons', methods=['GET'])
+def get_lessons():
+    data = fetch_data_from_table('lessons')
+    return jsonify(data)
+
+@app.route('/parents', methods=['GET'])
+def get_parents():
+    data = fetch_data_from_table('parents')
+    return jsonify(data)
+
+@app.route('/quizzes', methods=['GET'])
+def get_quizzes():
+    data = fetch_data_from_table('quizzes')
+    return jsonify(data)
+
+@app.route('/quiz_questions', methods=['GET'])
+def get_quiz_questions():
+    data = fetch_data_from_table('quiz_questions')
+    return jsonify(data)
+
+@app.route('/students', methods=['GET'])
+def get_students():
+    data = fetch_data_from_table('students')
+    return jsonify(data)
+
+@app.route('/student_quizzes', methods=['GET'])
+def get_student_quizzes():
+    data = fetch_data_from_table('student_quizzes')
+    return jsonify(data)
+
+@app.route('/teachers', methods=['GET'])
+def get_teachers():
+    data = fetch_data_from_table('teachers')
+    return jsonify(data)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    data = fetch_data_from_table('users')
+    return jsonify(data)
 
 
 if __name__ == '__main__':
