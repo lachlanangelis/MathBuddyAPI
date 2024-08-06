892cfd7e67abdf8181272a2b3e59fa91f50c9116
Lachlan Angelis <57650891+lachlanangelis@users.noreply.github.com>
2024-08-06 20:47:09 +1000

    added comments for student routes

--- Routes/student_routes.py
+++ Routes/student_routes.py
@@ -3,4 +3,14 @@ from flask import Blueprint, jsonify, current_app
 
 student_routes = Blueprint('student_routes', __name__)
 
+# Route to get students quiz list: includes completions, scores, ai feedback.
+
+# Route to get students current quiz, this should list the questions. 
+
+# Route to save a quiz, this includes marking the quiz, sending the prompt for ai feedback storing the result.
+
+# Route to modify student data including, Name, Gender, DOB.
+
+# Route to get student feedback, this should have the quizz name and the result/feedback associated
+
 @student_routes.route()
