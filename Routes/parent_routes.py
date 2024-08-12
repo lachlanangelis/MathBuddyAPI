b46dbd9e30214b3f55b4eac1428299c2a9c1c986
Zuhayer Aousaf <52472584+xuhayer@users.noreply.github.com>
2024-08-12 16:12:01 +1000

    Added skeleton parent routes

--- Routes/parent_routes.py
+++ Routes/parent_routes.py
@@ -6,3 +6,15 @@ parent_routes = Blueprint('parent_routes', __name__)
 def get_mysql():
     return current_app.config['mysql']
 
+#Route to display the child information of the parent
+
+#Route to display pending tasks of the child 
+
+#Route to display Progress report
+
+#Route to display Child feedback
+
+#Route to display parent information
+
+#Route to edit parent information
+
